62e46d8470945bf1f157ef04f39391f23f111e04
carolcortes <caroline.ocortes@gmail.com>
2022-05-20 17:05:22 -0300

    requisito 1 alterado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,10 +3,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   if (!ids) return ([]);
-  return ids.map((animalId) => data.species.find(({ id }) => (animalId === id)));
+  return ids.map((animalId) => species.find(({ id }) => (animalId === id)));
 }
-
-console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae',
-  '0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
