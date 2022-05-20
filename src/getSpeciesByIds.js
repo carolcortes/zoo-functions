fe8bd3727844e0713b757b4978ee0faf68a9afbf
carolcortes <caroline.ocortes@gmail.com>
2022-05-20 18:30:02 -0300

    retira if

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,6 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  if (!ids) return ([]);
   return ids.map((animalId) => species.find(({ id }) => (animalId === id)));
 }
 
