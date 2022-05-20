1c0d1e439477a7c51a46b7915be3f49c75d12b87
carolcortes <caroline.ocortes@gmail.com>
2022-05-20 18:45:49 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,9 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.find((specie) => specie.name === animal);
-  // return species.some((specie) => specie.name === animal && specie.residents.age >= age);
+  return species.find(({ name }) => (name === animal)).residents
+    .every(({ age: animalAge }) => animalAge >= age);
 }
 
-console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
