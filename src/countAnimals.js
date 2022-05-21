6a9fe9732cb7c9714c80a4b7abd7935577ffff64
carolcortes <caroline.ocortes@gmail.com>
2022-05-21 16:00:25 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const allAnimals = () => {
+  const info = {};
+  species.forEach(({ name, residents }) => {
+    info[name] = residents.length;
+  });
+  return info;
+};
+
+const countAnimals = (animal) => {
+  if (!animal) return allAnimals();
+  if (!animal.sex) return species.find(({ name }) => name === animal.specie).residents.length;
+  return species.find(({ name }) => name === animal.specie).residents
+    .filter(({ sex }) => sex === animal.sex).length;
+};
 
 module.exports = countAnimals;
