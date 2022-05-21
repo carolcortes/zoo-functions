51d33f087a86689cee37645ff2ee3389c5b03a05
carolcortes <caroline.ocortes@gmail.com>
2022-05-21 16:37:45 -0300

    requisito 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,38 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  it('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+
+  it('retorna a idade média dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('retorna undefined, caso não seja passado nenhum parâmetro', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('caso o parâmetro não seja uma string válida, retorna null', () => {
+    expect(handlerElephants('exemple')).toBeNull();
+  });
+
+  it('caso o parâmetro não seja uma string, retorna mensagem de erro', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+});
