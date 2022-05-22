3e1c0521764e93569fd7a34e429b4e3f67bbf2f8
carolcortes <caroline.ocortes@gmail.com>
2022-05-22 13:56:20 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui!
-}
+const countEntrants = (entrants) => ({
+  child: entrants.filter(({ age }) => age < 18).length,
+  adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+  senior: entrants.filter(({ age }) => age >= 50).length,
+});
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const calculateEntry = (entrants) => {
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const { child: countChild, adult: countAdult, senior: countSenior } = countEntrants(entrants);
+  const { child, adult, senior } = data.prices;
+  return child * countChild + adult * countAdult + senior * countSenior;
+};
 
 module.exports = { calculateEntry, countEntrants };
