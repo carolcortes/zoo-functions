80e37e08d780e2caca682094545b70732f783921
carolcortes <caroline.ocortes@gmail.com>
2022-05-22 22:26:30 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const getOldestFromFirstSpecies = (id) => {
+  const specie = employees.find(({ id: employeeId }) => employeeId === id).responsibleFor[0];
+  const oldestAnimal = species.find(({ id: animalId }) => animalId === specie).residents
+    .reduce((acc, resident) => (acc.age > resident.age ? acc : resident), 0);
+  return Object.values(oldestAnimal);
+};
 
 module.exports = getOldestFromFirstSpecies;
