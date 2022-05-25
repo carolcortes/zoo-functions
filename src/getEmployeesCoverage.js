5cc7212458eb96a447a842b05fddf27f355ee17d
carolcortes <caroline.ocortes@gmail.com>
2022-05-24 23:38:13 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,25 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const findEmployee = (info) => employees.find(({ id, firstName, lastName }) =>
+  info === id || info === firstName || info === lastName);
+
+const getSpeciesInfo = ({ responsibleFor }, key) => species.filter(({ id }) => responsibleFor
+  .includes(id)).map((animal) => animal[key]);
+
+const employeesInfo = (person) => ({
+  id: person.id,
+  fullName: `${person.firstName} ${person.lastName}`,
+  species: getSpeciesInfo(person, 'name'),
+  locations: getSpeciesInfo(person, 'location'),
+});
+
+const getAllEmployees = () => employees.map((employee) => employeesInfo(employee));
+
+const getEmployeesCoverage = (employee) => {
+  if (!employee) return getAllEmployees();
+  const searchInfo = findEmployee(Object.values(employee)[0]);
+  if (!searchInfo) throw new Error('Informações inválidas');
+  return employeesInfo(searchInfo);
+};
 
 module.exports = getEmployeesCoverage;
