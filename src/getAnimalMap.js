f28e7f2f56217f9a030381195a158e39877ca0c7
carolcortes <caroline.ocortes@gmail.com>
2022-05-26 23:24:52 -0300

    referencia

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,26 +1,29 @@
 const { species } = require('../data/zoo_data');
 
-const getName = (residents) => residents.map(({ name }) => name);
+const getResidentsName = (residents) => residents.map(({ name }) => name);
 
-const maleOrFemale = (sex, residents) => residents.filter(({ sex: animal }) => sex === animal)
+const getMaleOrFemale = (sex, residents) => residents.filter(({ sex: animal }) => sex === animal)
   .map(({ name }) => name);
 
 const getOptions = (sorted, sex, residents) => {
-  if (!sorted && !sex) return getName(residents);
-  if (!sorted) return maleOrFemale(sex, residents);
-  if (!sex) return getName(residents).sort();
-  return maleOrFemale(sex, residents).sort();
+  if (!sorted && !sex) return getResidentsName(residents);
+  if (!sorted) return getMaleOrFemale(sex, residents);
+  if (!sex) return getResidentsName(residents).sort();
+  return getMaleOrFemale(sex, residents).sort();
 };
 
-const animalInfo = (includeNames, sorted, sex) => species.reduce((acc,
+const createMap = (includeNames, sorted, sex) => species.reduce((acc,
   { location, name, residents }) => ({ ...acc,
   [location]: [...(acc[location] || []), (!includeNames
     ? name : { [name]: getOptions(sorted, sex, residents) })] }), {});
 
 const getAnimalMap = (options) => {
-  if (!options || !options.includeNames) return animalInfo();
+  if (!options || !options.includeNames) return createMap();
   const { includeNames, sorted, sex } = options;
-  return animalInfo(includeNames, sorted, sex);
+  return createMap(includeNames, sorted, sex);
 };
+
+// referência: https://dev.to/_bigblind/quick-tip-transform-an-array-into-an-object-using-reduce-2gh6
+// Agradecimento especial ao colega de tribo e (super) amigo Renan :)
 
 module.exports = getAnimalMap;
