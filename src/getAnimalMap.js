e205970817c8dab72c5bfc4603c51278e1a23ef1
carolcortes <caroline.ocortes@gmail.com>
2022-05-27 00:24:31 -0300

    code review matheus

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,29 +1,30 @@
 const { species } = require('../data/zoo_data');
 
-const getResidentsName = (residents) => residents.map(({ name }) => name);
+const getName = ({ name }) => name;
+const matchesSex = (sex) => ({ sex: animal }) => sex === animal;
+const getResidentsName = (residents) => residents.map(getName);
+const getResidentsBySex = (sex, residents) => residents.filter(matchesSex(sex));
 
-const getMaleOrFemale = (sex, residents) => residents.filter(({ sex: animal }) => sex === animal)
-  .map(({ name }) => name);
-
-const getOptions = (sorted, sex, residents) => {
+const handleOptions = ({ sorted, sex, residents }) => {
   if (!sorted && !sex) return getResidentsName(residents);
-  if (!sorted) return getMaleOrFemale(sex, residents);
+  if (!sorted) return getResidentsBySex(sex, residents).map(getName);
   if (!sex) return getResidentsName(residents).sort();
-  return getMaleOrFemale(sex, residents).sort();
+  return getResidentsBySex(sex, residents).map(getName).sort();
 };
 
-const createMap = (includeNames, sorted, sex) => species.reduce((acc,
+const createMap = ({ includeNames, sorted, sex }) => species.reduce((acc,
   { location, name, residents }) => ({ ...acc,
   [location]: [...(acc[location] || []), (!includeNames
-    ? name : { [name]: getOptions(sorted, sex, residents) })] }), {});
+    ? name : { [name]: handleOptions({ sorted, sex, residents }) })] }), {});
 
 const getAnimalMap = (options) => {
   if (!options || !options.includeNames) return createMap();
   const { includeNames, sorted, sex } = options;
-  return createMap(includeNames, sorted, sex);
+  return createMap({ includeNames, sorted, sex });
 };
 
 // referência: https://dev.to/_bigblind/quick-tip-transform-an-array-into-an-object-using-reduce-2gh6
 // Agradecimento especial ao colega de tribo e (super) amigo Renan :)
+// Metade de um code-review por Matheus Inácio :)
 
 module.exports = getAnimalMap;
